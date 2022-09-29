5117dcd3d86c8337a2b77e5b79b6205abce03dcb
chenchao <k82479@nbcb.com>
2022-09-29 10:31:09 +0800

    [1] 提交libtest

---
+++ group_lib/lib_a/src/main/java/com/example/lib_a/LibAUtil.java
@@ -0,0 +1,21 @@
+package com.example.lib_a;
+
+/**
+ * {类说明}
+ * <p>
+ * update_{时间}_{项目编号}_{开发者}
+ * 描述：{描述}
+ * 风险：{风险}
+ * <p>
+ * create_2022/9/29 10:22_{项目编号}_K82479_chenchao
+ * 描述：{描述}
+ * 风险：{风险}
+ *
+ * @XXX
+ * @XXX
+ */
+public class LibAUtil {
+    public static String printMsg(){
+        return "我是lib_a";
+    }
+} 
